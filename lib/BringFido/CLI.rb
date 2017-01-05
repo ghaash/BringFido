38a050ae1e31b33ca1f4924ae177df4bef625ff2
Daniel Silverman <daniel.silverman@me.com>
2017-01-05 02:24:50 +0000

    added functionality to CLI.rb

--- lib/BringFido/CLI.rb
+++ lib/BringFido/CLI.rb
@@ -2,14 +2,25 @@
 
 class BringFido::CLI
 
-def call
-puts "Don't you forget to bring your doggo"
-list_options
-end
+  def call
+  puts "Don't you forget to bring your doggo"
+  list_options
+  menu
+  end
 
-def list_options
-puts "the 3x8 grid of what I want to scrape"
-end
+  def list_options
+  puts "the 3x8 grid of what I want to scrape"
+  end
 
+  def menu
+  puts "Where do you want to bring Fido today? Pick a number"
+  input = gets.strip
+  case input
+  when "1"
+    puts "info on 1st 3x8"
+  when "2"
+    puts "info on 2 3x8"
+  end
+  end
 
 end

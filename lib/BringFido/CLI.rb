1d1a06cb813665b15adc6fd040483c5fedc8f204
Daniel Silverman <daniel.silverman@me.com>
2017-01-05 04:49:40 +0000

    dishing out the parks

--- lib/BringFido/CLI.rb
+++ lib/BringFido/CLI.rb
@@ -12,23 +12,34 @@ class BringFido::CLI
   def list_options
     puts "the 3x8 grid of what I want to scrape"
     @bring = BringFido::Bring.parks
+    @bring.each.with_index(1) do |bring, i|
+      puts "#{i}. #{bring}"
+    end
   end
 
   def menu
     input = nil
       while input != "exit"
          puts "Where do you want to bring Fido today? Pick a number or type 'list' to see a list, and lastly type 'exit' to exit"
-      input = gets.strip
-        case input
-          when "1"
-            puts "info on 1st 3x8"
-          when "2"
-            puts "info on 2 3x8"
-        when "list"
-          puts list_options
-        end
-      end
+      input = gets.strip.downcase
+      if input.to_i > 0
+        puts @bring[input.to_i-1]
+      elsif input == "list"
+        list_options
+    else
+      puts "you can't use a computer doggo!"
     end
+#        case input
+#          when "1"
+#            puts "info on 1st 3x8"
+#          when "2"
+#            puts "info on 2 3x8"
+#        when "list"
+#          puts list_options
+#        end
+#      end
+    end
+  end
 
   def end_menu
     puts "You and your doggo are welcome any time!"
--- lib/BringFido/bring.rb
+++ lib/BringFido/bring.rb
@@ -5,12 +5,26 @@ class BringFido::Bring
   def self.parks
     #this method needs to return the scraped dog parks
     puts "oh look at these pawsome parks!"
+
     park_1 = self.new
     park_1.name = "Grape Street Dog Park"
     park_1.bones = "5 bones!"
     park_1.location = "San Diego, CA, US"
     park_1.url = "https://www.bringfido.com/attraction/parks/ "
-    [park_1]
+
+    park_2 = self.new
+    park_2.name = "Nate's Point Dog Park"
+    park_2.bones = "3 bones!"
+    park_2.location = "San Diego, CA, US"
+    park_2.url = "https://www.bringfido.com/attraction/parks/ "
+
+    park_3 = self.new
+    park_3.name = "Kearney Mesa Dog ParK"
+    park_3.bones = "5 bones!"
+    park_3.location = "San Diego, CA, US"
+    park_3.url = "https://www.bringfido.com/attraction/parks/ "
+
+    [park_1, park_2, park_3]
   end
 
 end

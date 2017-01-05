4da66930145557ed02fc9cd315b6805f1492de50
Daniel Silverman <daniel.silverman@me.com>
2017-01-05 04:23:23 +0000

    make changes to requires and added layout for scrap specifics

--- lib/BringFido/bring.rb
+++ lib/BringFido/bring.rb
@@ -1,8 +1,16 @@
 class BringFido::Bring
 
+  attr_accessor :name, :bones, :location, :url
+
   def self.parks
     #this method needs to return the scraped dog parks
     puts "oh look at these pawsome parks!"
+    park_1 = self.new
+    park_1.name = "Grape Street Dog Park"
+    park_1.bones = "5 bones!"
+    park_1.location = "San Diego, CA, US"
+    park_1.url = "https://www.bringfido.com/attraction/parks/ "
+    [park_1]
   end
 
 end

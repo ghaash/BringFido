08f666d8433d59edc25e0c0ca091f06ac5db586c
Daniel Silverman <daniel.silverman@me.com>
2017-01-16 16:35:43 +0000

    redid iteration for formatting

--- lib/BringFido/CLI.rb
+++ lib/BringFido/CLI.rb
@@ -3,31 +3,44 @@
 class BringFido::CLI
 
   def call
-    puts "Don't you forget to bring your doggo"
-    list_options
+    puts "Don't you forget to bring your doggo!"
+    #list_options
     menu
     end_menu
   end
 
   def list_options
-    puts "Here is a list of parks followed by their locations!"
+    puts "Here is a list of parks!"
     @bring = BringFido::Bring.parks
-    @bring.each.with_index(1) do |park, i|
+    @bring.each.with_index(1) do |bring, i|
       # binding.pry
-      puts "#{i}. #{park.name} #{park.location} #{park.description}"
+      puts "#{i}. #{bring.name} + #{bring.location} -- #{bring.description}"
     end
   end
 
   def menu
     input = nil
       while input != "exit"
-         puts "Look at all these parks! Type 'list' to see a list, type 'exit' to exit"
+         puts "Where do you want to bring Fido today? Type 'parks' to see the list of park names, type 'location' to see a list of their locations, type 'description' to see a list of their descriptions, type 'exit' to exit"
       input = gets.strip.downcase
-      if input.to_i > 0
-        the_parks = @bring[input.to_i-1]
-        puts "#{the_parks.name} #{the_parks.location} #{the_parks.description}"
-      elsif input == "list"
-        list_options
+      if input == "parks"
+        @bring = BringFido::Bring.parks
+        @bring.each.with_index(1) do |bring|
+        puts "#{bring.name}"
+      end
+    elsif input == "location"
+      @bring = BringFido::Bring.parks
+      @bring.each.with_index(1) do |bring|
+      puts "#{bring.location}"
+    end
+    #  if input.to_i > 0
+      #  the_bring = @bring[input.to_i-1]
+      #  puts "#{the_bring.name} --- #{the_bring.location} --- #{the_bring.description}"
+    elsif input == "description"
+          @bring = BringFido::Bring.parks
+          @bring.each.with_index(1) do |bring|
+          puts "#{bring.description}"
+        end
     else
       puts "You don't how to use a computer doggo! Go get your humane!"
     end
@@ -44,7 +57,7 @@ class BringFido::CLI
   end
 
   def end_menu
-    puts "Make your dog happy, go to the park!"
+    puts "Make your dog happy by taking him to the park!"
   end
 
 end

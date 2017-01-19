9de766c439c535ad13e0f0712d2db3afafc1eaf2
Daniel Silverman <daniel.silverman@me.com>
2017-01-19 03:03:54 +0000

    added gsub to description

--- lib/BringFido/scraper.rb
+++ lib/BringFido/scraper.rb
@@ -19,7 +19,8 @@ doc = Nokogiri::HTML(open("https://www.bringfido.com/attraction/parks/"))
 parks = self.new
 parks.name = doc.xpath("//div[@class='info-ctn']/div[@class='info']/h1/a").text
 parks.location = doc.xpath("//div[@class='info-ctn']/div[@class='info']/div[@class='subtitle']/a").text
-parks.description = doc.xpath("//div[@class='info-ctn']/div[@class='overlay-info']/div[@class='description character-limit']/@text").text
+parks.description = doc.xpath("//div[@class='info-ctn']/div[@class='overlay-info']/div[@class='description character-limit']/@text").text.gsub(/[.]/, '.
+')
 parks
 end
 end

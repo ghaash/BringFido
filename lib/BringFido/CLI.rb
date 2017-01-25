94cc6e1a884169e3114214dde70cfb19d356195e
Daniel Silverman <daniel.silverman@me.com>
2017-01-25 22:41:13 +0000

    UI changes finished!

--- lib/BringFido/CLI.rb
+++ lib/BringFido/CLI.rb
@@ -2,9 +2,9 @@ class BringFido::CLI
 
   def call
     puts ""
-    puts "Don't you forget to bring your doggo!"
-    puts ""
     display_parks
+    puts ""
+    puts "Don't you forget to bring your doggo!"
     menu
     end_menu
   end
@@ -21,21 +21,20 @@ end
       while input != "exit"
         puts ""
          puts "Where do you want to bring Fido today? Choose a number to see where the park is located and a description, or type 'exit' to exit"
+         puts ""
       input = gets.strip.downcase
       if input == "parks"
         display_parks
-    elsif
-      more_info(input)
     else
-      puts ""
-      puts "You don't how to use a computer doggo! Go get your humane!"
-      puts ""
+      more_info(input)
     end
   end
 end
 
   def end_menu
+    puts ""
     puts "Make your dog happy by taking him to the park!"
+    puts ""
   end
 
   def more_info(input)
